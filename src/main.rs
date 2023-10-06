1cbfa3f0d767d3a873b49cf7086e68f6fcdeadf6
selfharm777@protonmail.com <selfharm777@protonmail.com>
2023-10-06 12:32:26 +0100

    comments, total time spent compressing: ms

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,22 @@ use std::io::BufReader;
 use std::time::Instant;
 
 fn main() {
+    //checking for cmd_l args to >= 3
     if args().len() != 3 {
         eprintln!("Usage: `source` `target`");
         return;
     }
+    //open file specified as the 2nd arg (and wrap)
     let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    //create new file as (3rd arg)
     let output = File::create(args().nth(2).unwrap()).unwrap();
+    //compressing>>>
     let mut encoder = GzEncoder::new(output, Compression::default());
+
     let start = Instant::now();
+
+    let duration = start.duration_since(start);
+
     copy(&mut input, &mut encoder).unwrap();
     let output = encoder.finish().unwrap();
     println!(
@@ -25,4 +33,8 @@ fn main() {
     );
     println!("Target len: {:?}", output.metadata().unwrap().len());
     println!("Elapsed: {:?}", start.elapsed());
+    println!(
+        "Spent: {:?} milliseconds compressing this file",
+        duration.as_millis()
+    );
 }
